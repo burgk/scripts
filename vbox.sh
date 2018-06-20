5daf01790676ebc6fae9812979dd50b5074d2d7c
Kevin Burg - OIT <kevin.burg@state.co.us>
2018-06-20 14:49:20 -0600

    Fully populated the color options in vbox.sh

--- vbox.sh
+++ vbox.sh
@@ -3,12 +3,17 @@
 # Script to query status and start/stop specific VirtualBox VMs
 
 VBOX=VBoxManage.exe
+
+# Set color constants
 RED="\033[1;31m"
-YELLOW="\033[1;33m"
 GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+MAGENTA="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
 BOLD="\033[1m"
 RESET="\033[0m"
-
 
 if [ "$1" = "list" ]
 then
