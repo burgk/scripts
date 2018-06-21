796e91b072ed601c2f46b3c37c993a8b8ddf5a1b
Kevin Burg - OIT <kevin.burg@state.co.us>
2018-06-21 11:27:17 -0600

    Add python example script

---
+++ rsg.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Random Selection Game
+# Taken from https://sites.google.com/a/state.co.us/oit-plaza-dev/home/training/classes/piday
+
+import random
+
+entrants = ["Lillian", "William", "Nancy", "James", "Deb", "Brian"]
+
+first_prize = random.randrange(0,len(entrants))
+
+second_prize = random.randrange(0,len(entrants))
+while second_prize == first_prize:
+    second_prize = random.randrange(0,len(entrants))
+third_prize = random.randrange(0,len(entrants))
+while third_prize == first_prize or third_prize == second_prize:
+    third_prize = random.randrange(0,len(entrants))
+print "Selections are done and:"
+print "1st Prize, a Raspberry Pi 3, goes to", entrants[first_prize]
+print "2nd Prize, a Raspberry Pi Zero, goes to", entrants[second_prize]
+print "3rd Prize, a gift card to Micro Center, goes to", entrants[third_prize]
