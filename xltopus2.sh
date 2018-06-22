c4338ac95220f9af6c5e72195cbf1d6393f0ec62
Kevin Burg - OIT <kevin.burg@state.co.us>
2018-06-22 12:11:34 -0600

    Refined bitrate selection code

--- xltopus2.sh
+++ xltopus2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Kevin Burg - burg.kevin@gmail.com
+
 # Wrapper for ffmpeg to convert .ogg files to .mp3 files
 # lame quality settings:
 # 0 = ~245 - range: 220-260
@@ -18,37 +18,38 @@ IFS='\'
 INPUT=$1
 IFILEBASE=${1%%opus}
 OUTPUT=${IFILEBASE}mp3
-BITRATE=$(mediainfo ${INPUT} | grep "Overall bit rate" | cut -d: -f2 | cut -d' ' -f2)
+# BITRATE=$(mediainfo ${INPUT} | grep "Overall bit rate" | cut -d: -f2 | cut -d' ' -f2)
+BITRATE=$(mediainfo ${INPUT} | grep -E 'Overall.*kb/s' | sed 's/[^0-9]//g')
 
 if [ ${BITRATE} -lt "65" ]
-then CODECSCALE=9
+ then CODECSCALE=9
 elif [ ${BITRATE} -lt "85" ]
-then CODECSCALE=8
+ then CODECSCALE=8
 elif [ ${BITRATE} -lt "100" ]
-then CODECSCALE=7
+ then CODECSCALE=7
 elif [ ${BITRATE} -lt "115" ]
-then CODECSCALE=6
+ then CODECSCALE=6
 elif [ ${BITRATE} -lt "130" ]
-then CODECSCALE=5
+ then CODECSCALE=5
 elif [ ${BITRATE} -lt "165" ]
-then CODECSCALE=4
+ then CODECSCALE=4
 elif [ ${BITRATE} -lt "175" ]
-then CODECSCALE=3
+ then CODECSCALE=3
 elif [ ${BITRATE} -lt "190" ]
-then CODECSCALE=2
+ then CODECSCALE=2
 elif [ ${BITRATE} -lt "225" ]
-then CODECSCALE=1
+ then CODECSCALE=1
 elif [ ${BITRATE} -lt "245" ]
-then CODECSCALE=0
+ then CODECSCALE=0
 fi
 
 if [ $# -eq 1 ]
-then
-	echo -e "Encoding ${INPUT} to ${OUTPUT} at ${CODECSCALE}"
-$FF -i ${INPUT} -acodec libmp3lame -qscale:a ${CODECSCALE}  -map_metadata 0:s:0 ${OUTPUT}
+ then
+  echo -e "Encoding ${INPUT} to ${OUTPUT} at ${CODECSCALE}"
+   $FF -i ${INPUT} -acodec libmp3lame -qscale:a ${CODECSCALE}  -map_metadata 0:s:0 ${OUTPUT}
 exit 0
 
 else
-echo "Usage: $(basename $0) <input.file>" 
-exit 1
+ echo "Usage: $(basename $0) <input.file>" 
+ exit 1
 fi
