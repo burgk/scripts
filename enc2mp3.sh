2882a0400cfd79d48d1ad4ab4324b0d01f27540b
Kevin Burg <burg.kevin@gmail.com>
2018-06-29 12:20:18 -0600

    Misc code cleanup

--- enc2mp3.sh
+++ enc2mp3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Kevin Burg - burg.kevin@gmail.com
-# Wrapper for ffmpeg to convert .ogg files to .mp3 files
+# Wrapper for ffmpeg to convert ogg and opus files to .mp3 files
 
 # libmp3lame quality settings:
 # 0 = ~245 - range: 220-260
@@ -13,6 +13,15 @@
 # 7 = ~100 - range: 80-120
 # 8 = ~85 - range: 70-105
 # 9 = ~65 - range: 45-85
+
+IFS='\'
+INPUT=$1
+INFILEBASE=${1%.*}
+INFILEEXT=${1##*.}
+OUTPUT=${INFILEBASE}.mp3
+F_RED="\e[38;2;255;0;0m"
+F_GREEN="\e[38;2;0;255;0m"
+RESET="\e[0m"
 
 which ffmpeg > /dev/null 2>&1
 if (( $? == 0 ))
@@ -31,20 +40,6 @@ if (( $? == 0 ))
   echo -e "${F_RED}mediainfo not found, exiting${RESET}"
   exit 1
 fi
-
-# FF=/usr/bin/ffmpeg
-# FF=$(which ffmpeg)
-IFS='\'
-INPUT=$1
-# IFILEBASE=${1%%opus}
-INFILEBASE=${1%.*}
-INFILEEXT=${1##*.}
-OUTPUT=${INFILEBASE}.mp3
-# BITRATE=$(mediainfo ${INPUT} | grep "Overall bit rate" | cut -d: -f2 | cut -d' ' -f2)
-# BITRATE=$(mediainfo ${INPUT} | grep -E 'Overall.*kb/s' | sed 's/[^0-9]//g')
-F_RED="\e[38;2;255;0;0m"
-F_GREEN="\e[38;2;0;255;0m"
-RESET="\e[0m"
 
 setcodescale()
 {
