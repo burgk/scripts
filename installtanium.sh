17d4e041b6731edb5aa5ac08bd087cbc2215c8de
Kevin Burg <burg.kevin@gmail.com>
2019-01-24 16:51:24 -0700

    Updates to the installtanium universal installer

--- installtanium.sh
+++ installtanium.sh
@@ -12,7 +12,7 @@ f_red="\e[38;2;255;0;0m"
 f_green="\e[38;2;0;255;0m"
 reset="\e[0m"
 # Tanium packages and files
-taniumpub="tanium.pub"
+taniumpub="./tanium.pub"
 debian67_32="./taniumclient_7.2.314.3476-debian6_i386.deb"
 debian67_64="./taniumclient_7.2.314.3476-debian6_amd64.deb"
 debian8_32="./taniumclient_7.2.314.3476-debian8_i386.deb"
@@ -309,9 +309,93 @@ else
 fi
 } #}}}
 
+validate_package(){ #{{{
+installer=$0
+case ${installer} in
+  ./InstallTaniumRHEL.sh)
+    if [[ -e ${installpkg} ]]; then
+      pkg_exists=true
+    else
+      echo -e "${f_red}Install package ${installpkg} not found in directory, exiting${reset}"
+      exit 1
+    fi
+    if [[ -e ${taniumpub} ]]; then
+      pub_exists=true
+    else
+      echo -e "${f_red}Install pub ${taniumpub} not found in directory, exiting${reset}"
+      exit 1
+    fi
+  ;;
+  ./InstallTaniumOracle.sh)
+    if [[ -e ${installpkg} ]]; then
+      pkg_exists=true
+    else
+      echo -e "${f_red}Install package ${installpkg} not found in directory, exiting${reset}"
+      exit 1
+    fi
+    if [[ -e ${taniumpub} ]]; then
+      pub_exists=true
+    else
+      echo -e "${f_red}Install pub ${taniumpub} not found in directory, exiting${reset}"
+      exit 1
+    fi
+  ;;
+  ./InstallTaniumSUSE.sh)
+    if [[ -e ${installpkg} ]]; then
+      pkg_exists=true
+    else
+      echo -e "${f_red}Install package ${installpkg} not found in directory, exiting${reset}"
+      exit 1
+    fi
+    if [[ -e ${taniumpub} ]]; then
+      pub_exists=true
+    else
+      echo -e "${f_red}Install pub ${taniumpub} not found in directory, exiting${reset}"
+      exit 1
+    fi
+  ;;
+  ./InstallTaniumDebian.sh)
+    if [[ -e ${installpkg} ]]; then
+      pkg_exists=true
+    else
+      echo -e "${f_red}Install package ${installpkg} not found in directory, exiting${reset}"
+      exit 1
+    fi
+    if [[ -e ${taniumpub} ]]; then
+      pub_exists=true
+    else
+      echo -e "${f_red}Install pub ${taniumpub} not found in directory, exiting${reset}"
+      exit 1
+    fi
+  ;;
+  ./InstallTaniumUbuntu.sh)
+    if [[ -e ${installpkg} ]]; then
+      pkg_exists=true
+    else
+      echo -e "${f_red}Install package ${installpkg} not found in directory, exiting${reset}"
+      exit 1
+    fi
+    if [[ -e ${taniumpub} ]]; then
+      pub_exists=true
+    else
+      echo -e "${f_red}Install pub ${taniumpub} not found in directory, exiting${reset}"
+      exit 1
+    fi
+  ;;
+  * )
+    echo -e "${f_red}Unrecognized install script ${installer}, exiting${reset}"
+    exit 1
+esac
+
+} #}}}
+
 get_domain() { #{{{
- tmpdomain=$(hostname -d | awk -F'.' '{print $1}')
- domain=${tmpdomain^^} # not supported in bash < 4
+tmpdomain=$(hostname -d | awk -F'.' '{print $1}')
+if [[ -z ${tmpdomain} ]]; then
+  domain=${tmpdomain^^} # not supported in bash < 4
+else
+  domain=Unconfigured
+fi 
 } #}}}
 
 show_banner() { #{{{
@@ -322,28 +406,198 @@ echo -e '      / / / /| | /  |/ // // / / / /|_/ /'
 echo -e '     / / / ___ |/ /|  // // /_/ / /  / /'
 echo -e '    /_/ /_/  |_/_/ |_/___/\____/_/  /_/'
 echo -e "${reset}\n"
+if [[ ${supported_distro} = "true" ]] && [[ ${supportedver} = "true" ]] && [[ ${supportedarch} = "true" ]] && [[ ${pkg_exists} = "true" ]] && [[ ${pub_exists} = "true" ]]; then
+  echo -e "Found distribution: ${distro}"
+  echo -e "Supported distro: ${supported_distro}"
+  echo -e "Found version: ${majversion}"
+  echo -e "Supported version: ${supportedver}"
+  echo -e "Found architecture: ${architecture}"
+  echo -e "Supported architecture: ${supportedarch}"
+  echo -e "Found install package: ${installpkg}"
+  echo -e "Found Tanium key file: ${taniumpub}"
+# echo -e "Suggest agency is ${domain}"
+  echo -e "${f_green}This is a supported configuration, continuing...${reset}\n"
+else
+  echo -e "${f_red}This is NOT a supported configuration, exiting.${reset}"
+  exit 1
+fi
 } #}}}
 
+prompt_agency() { #{{{
+echo -e "This script supports the following Agencies:"
+echo -e "  1 - CDA \t\t 11 - DOC"
+echo -e "  2 - CDHS \t\t 12 - DOLA"
+echo -e "  3 - CDLE \t\t 13 - DOR"
+echo -e "  4 - CDOT \t\t 14 - DORA"
+echo -e "  5 - CDPHE \t\t 15 - DPA"
+echo -e "  6 - CDPS \t\t 16 - GOV"
+echo -e "  7 - CHS \t\t 17 - HCPF"
+echo -e "  8 - CST \t\t 18 - OIT"
+echo -e "  9 - DMVA \t\t 19 - OITEDIT"
+echo -e " 10 - DNR\n"
+echo -n "Please enter agency number and press [ENTER]: "
+read response
+case ${response} in
+ 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 19)
+   serverip=${publicserverip}
+   ;;
+ 18)
+   serverip=${oitserverip}
+   ;;
+ *)
+   echo -e "${f_red}That is not a valid agency number, exiting.${reset}"
+   exit 1
+   ;;
+esac
+
+if [ ${response} -eq 1 ]
+ then agency="CDA"
+elif [ ${response} -eq 2 ]
+ then agency="CDHS"
+elif [ ${response} -eq 3 ]
+ then agency="CDLE"
+elif [ ${response} -eq 4 ]
+ then agency="CDOT"
+elif [ ${response} -eq 5 ]
+ then agency="CDPHE"
+elif [ ${response} -eq 6 ]
+ then agency="CDPS"
+elif [ ${response} -eq 7 ]
+ then agency="CHS"
+elif [ ${response} -eq 8 ]
+ then agency="CST"
+elif [ ${response} -eq 9 ]
+ then agency="DMVA"
+elif [ ${response} -eq 10 ]
+ then agency="DNR"
+elif [ ${response} -eq 11 ]
+ then agency="DOC"
+elif [ ${response} -eq 12 ]
+ then agency="DOLA"
+elif [ ${response} -eq 13 ]
+ then agency="DOR"
+elif [ ${response} -eq 14 ]
+ then agency="DORA"
+elif [ ${response} -eq 15 ]
+ then agency="DPA"
+elif [ ${response} -eq 16 ]
+ then agency="GOV"
+elif [ ${response} -eq 17 ]
+ then agency="HCPF"
+elif [ ${response} -eq 18 ]
+ then agency="OIT"
+elif [ ${response} -eq 19 ]
+ then agency="OITEDIT"
+fi
+} #}}}
+
+install_package() { #{{{
+case ${distro} in
+ Redhat | CentOS | Oracle | SLES | openSUSE)
+  echo -e "Installing Tanium client for ${distro}, version ${majversion}"
+  rpm -ivh ${installpkg}
+  sleep 5
+;;
+ Debian | debian | Ubuntu)
+  echo -e "Installing Tanium client for ${distro}, version ${majversion}"
+  dpkg -i ${installpkg}
+  sleep 5
+;;
+ *)
+  echo -e "${f_red}Unrecognized distro, exiting. ${distro}${reset}"
+  exit 1
+;;
+esac
+} # }}}
+
+configure_tanium() { #{{{
+if [[ ${pub_exists} = "true" ]]; then
+  echo -e "Installing pub file: ${f_green}${taniumpub}${reset}"
+  cp ${taniumpub} /opt/Tanium/TaniumClient/
+elif [[ ${pub_exists} = "false" ]]; then
+  echo -e "${f_red}Tanium public key not found, exiting.${reset}"
+  exit 1
+fi
+echo -e "Setting Tanium parameters:"
+echo -e "  Tanium server IP: ${f_green}${serverip}${reset}"
+/opt/Tanium/TaniumClient/TaniumClient config set ServerNameList ${serverip}
+echo -e "  Tanium server port: ${f_green}${taniumport}${reset}"
+/opt/Tanium/TaniumClient/TaniumClient config set taniumport ${taniumport}
+echo -e "  Tanium log verbosity level: ${f_green}${verbosity}${reset}"
+/opt/Tanium/TaniumClient/TaniumClient config set LogVerbosityLevel ${verbosity}
+echo -e "Setting Agency custom tag to: ${f_green}${agency}${reset}"
+if [ -d /opt/Tanium/TaniumClient/Tools ]
+ then
+  echo -e "~~${agency}" > /opt/Tanium/TaniumClient/Tools/CustomTags.txt
+ else
+  mkdir /opt/Tanium/TaniumClient/Tools
+  echo -e "~~${agency}" > /opt/Tanium/TaniumClient/Tools/CustomTags.txt
+fi
+} #}}}
+
+start_services() { #{{{
+case ${distro} in
+ Redhat | CentOS | Oracle)
+  if [[ ${majversion} = "5" ]] || [[ ${majversion} = "6" ]]; then
+   echo -e "Starting Tanium service"
+   service TaniumClient start
+   chkconfig --level 2345 TaniumClient on
+  elif [[ ${majversion} = "7" ]]; then
+   echo -e "Starting Tanium service"
+    systemctl start taniumclient
+    systemctl enable taniumclient
+  fi
+ ;;
+ SUSE | openSUSE)
+  echo -e "Starting Tanium service"
+  service TaniumClient start
+  chkconfig --level 2345 TaniumClient on
+ ;;
+ Debian | debian)
+  if [[ ${majversion} = "6" ]] || [[ ${majversion} = "7" ]]; then
+   echo -e "Starting Tanium service"
+   service TaniumClient start
+   chkconfig --level 2345 TaniumClient on
+  elif [[ ${majversion} = "8" ]] || [[ ${majversion} = "9" ]]; then
+   echo -e "Starting Tanium Service"
+   systemctl start taniumclient
+   systemctl enable taniumclient
+  fi
+ ;;
+ Ubuntu)
+  if [[ ${majversion} = "10" ]] || [[ ${majversion} = "14" ]]; then
+   echo -e "Starting Tanium service"
+   service TaniumClient start
+   chkconfig --level 2345 TaniumClient on
+  elif [[ ${majversion} = "16" ]] || [[ ${majversion} = "18" ]]; then
+   echo -e "Starting Tanium Service"
+   systemctl start taniumclient
+   systemctl enable taniumclient
+  fi
+ ;;
+esac
+} #}}}
+
+final_message() { #{{{
+echo -e "${F_GREEN}Install Complete${RESET}"
+echo -e "${F_RED}Please verify that firewall port ${SERVERPORT}/TCP is open to ${SERVERIP}${RESET}"
+exit 0
+} #}}}
+
+# BEGIN PROCESSING
 # check_root
 get_distro
 get_distroversion
 validate_distroversion
 get_arch
 validate_arch
-get_domain
+# get_domain - UNUSED FOR NOW
 select_package
-# show_banner
-echo -e "Found distribution: ${distro}"
-echo -e "Supported distro: ${supported_distro}"
-echo -e "Found version: ${majversion}"
-echo -e "Supported version: ${supportedver}"
-echo -e "Found architecture: ${architecture}"
-echo -e "Supported arch: ${supportedarch}"
-# echo -e "Suggest agency is ${domain}"
-if [[ ${supported_distro} = "true" ]] && [[ ${supportedver} = "true" ]] && [[ ${supportedarch} = "true" ]]; then
-  echo -e "${f_green}This is a supported configuration${reset}"
-  echo -e "Will install ${installpkg}"
-else
-  echo -e "${f_red}This is NOT a supported configuration${reset}"
-fi
+validate_package
+show_banner
+prompt_agency
+# install_package
+# configure_tanium
+# start_services
+# final_message
 exit 0
