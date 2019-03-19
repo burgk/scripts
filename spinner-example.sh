ca1dfd8359cb5053b1c9acb8a4b8b22c14f15c15
Kevin Burg <burg.kevin@gmail.com>
2019-03-19 13:49:43 -0600

    Add spinner-example.sh

---
+++ spinner-example.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+while true
+do
+  printf "."
+  printf -- "-%.0s"
+  printf sleep 0.10
+
+  printf -- "\b \b\b%.0b"
+  printf -- "\\%.0s"
+  sleep 0.10
+
+  printf -- "\b%.0s"
+  printf -- "|%.0s"
+  sleep 0.10
+
+  printf -- "\b%.0s"
+  printf -- "/%.0s"
+  sleep 0.10
+
+  printf -- "\b%.0s"
+  printf -- "."
+done
+
