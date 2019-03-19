5ff3ee78f5270ce49422367d740dbf44eb5fa36c
Kevin Burg <burg.kevin@gmail.com>
2019-03-19 13:38:39 -0600

    Added oracle-users.sh file

---
+++ oracle-users.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to log who logs in to Oracle servers and how
+# Runs daily from /etc/cron.daily
+# Kevin Burg - kevin.burg@state.co.us
+
+logfile="/var/log/secure"
+resultfile="/home/burgk/oracle-users.txt"
+today="$(date +%b" "%d)"
+
+if [ -e "${logfile}" ]; then
+  echo -e "-----------------------------" >> "${resultfile}"
+  echo -e "Login Summary for: $(date +%F)" >> "${resultfile}"
+  echo -e "-----------------------------" >> "${resultfile}"
+  grep -w "${today}" "${logfile}" | grep "Accepted" | grep -v grep  >> "${resultfile}"
+else
+  echo -e "Log file not found!" >> "${resultfile}"
+  logger "oracle-users.sh - log file not found"
+  exit 1
+fi
+
+
