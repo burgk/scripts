54000bce7e9f17137b00bc37842c5df659d30044
Kevin Burg <burg.kevin@gmail.com>
2019-03-20 04:30:51 -0600

    Updated ssh-users to check for a specific user

--- ssh-users.sh
+++ ssh-users.sh
@@ -7,14 +7,20 @@
 # 59 23 * * * root /root/ssh-users.sh
 
 logfile="/var/log/secure"
-resultfile="${HOME}/ssh-users.txt"
+resultfile="/home/serveradmin/ssh-users.txt"
 today="$(date +%b" "%d)"
+checkuser="oracle"
 
 if [ -e "${logfile}" ]; then
-  echo -e "-----------------------------" >> "${resultfile}"
-  echo -e "Login Summary for: $(date +%F)" >> "${resultfile}"
-  echo -e "-----------------------------" >> "${resultfile}"
-  grep -w "${today}" "${logfile}" | grep "Accepted" | grep -v grep  >> "${resultfile}"
+  echo -e "------------------------------------" >> "${resultfile}"
+  echo -e "Oracle Login Summary for: $(date +%F)" >> "${resultfile}"
+  echo -e "------------------------------------" >> "${resultfile}"
+  if grep -w "${today}" "${logfile}" | grep "Accepted" | grep -i "${checkuser}" > /dev/null 2>&1; then
+    grep -w "${today}" "${logfile}" | grep "Accepted" | grep -i "${checkuser}" | grep -v grep  >> "${resultfile}"
+    echo -e "\n" >> "${resultfile}"
+  else
+    echo -e "No "${checkuser}" logins recorded today\n" >> "${resultfile}"
+  fi
 else
   echo -e "ERROR - $(date +%F): Log file not found" >> "${resultfile}"
   logger "ssh-users.sh: Error - Log file not found"
