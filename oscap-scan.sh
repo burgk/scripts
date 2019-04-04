c6af07582917a28cba20ad0db4280635728cbd23
Kevin Burg <burg.kevin@gmail.com>
2019-04-04 14:17:34 -0600

    Quick update

--- oscap-scan.sh
+++ oscap-scan.sh
@@ -9,21 +9,23 @@ reportfile="${HOME}/$(date +%F)-xccdf.html"
 # }}}
 
 # Misc function definitions {{{
-
 check_root() { #{{{ String comparison
 if [[ $EUID -ne 0 ]]; then
-  echo -e "You need to be use sudo or be root to run this script"
+  echo -e "You need to use sudo or be root to run this script"
   echo -e "For example: sudo ./$(basename ${0})"
   exit 1
  fi
 } #}}}
-
 # }}}
 
 # Begin main tasks {{{
 check_root
-oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig-rhel7-disa --results "${resultfile}" --report "${reportfile}" /usr/share/xml/scap/ssg/content/ssg-rhel7-ds.xml
-
+if [[ -e /usr/bin/oscap ]]; then
+  oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_stig-rhel7-disa --results "${resultfile}" --report "${reportfile}" /usr/share/xml/scap/ssg/content/ssg-rhel7-ds.xml
+else
+  echo -e "Error: oscap binary not found, exiting"
+  exit 1
+fi
 # }}}
 
 exit 0
