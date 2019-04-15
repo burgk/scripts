ebe010cb06fade60b958cfe3a9e2b33deaf95ac4
Kevin Burg <burg.kevin@gmail.com>
2019-04-15 10:57:48 -0600

    Added CDOT FedACH dl script

---
+++ dl-cdot-fedach.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# Purpose: Download CDOT FedACH doc
+# Date: 20190415
+# Kevin Burg - Kevin.Burg@state.co.us
+
+rundate=$(date +%F-%T)
+resultfile="/home/burgk/Documents/FedACH/${rundate}-CDOT-FedACH.txt"
+
+curl \
+--header "X_FRB_EPAYMENTS_DIRECTORY_ORG_ID: 121000248" \
+--header "X_FRB_EPAYMENTS_DIRECTORY_DOWNLOAD_CD: 3f2b553a-b3dc-4a2c-9213-f2b721142bbd" \
+https://frbservices.org/EPaymentsDirectory/directories/fedach?format=text > "${resultfile}"
