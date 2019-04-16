94171f1c14ad77100a93f27e184b3eae563d7b06
Kevin Burg <burg.kevin@gmail.com>
2019-04-16 10:28:42 -0600

    Updated CDOT FedACH dl script

--- dl-cdot-fedach.sh
+++ dl-cdot-fedach.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Purpose: Download CDOT FedACH doc
+# Purpose: Download CDOT FedACH data
 # Date: 20190415
 # Kevin Burg - Kevin.Burg@state.co.us
 
@@ -7,21 +7,21 @@
 destdir="/home/burgk/Documents/FedACH"
 rundate=$(date +%F-%H%M%S)
 resultfile="${destdir}/${rundate}-CDOT-FedACH.txt"
+header1="X_FRB_EPAYMENTS_DIRECTORY_ORG_ID: 121000248" 
+header2="X_FRB_EPAYMENTS_DIRECTORY_DOWNLOAD_CD: 3f2b553a-b3dc-4a2c-9213-f2b721142bbd"
+dl_url="https://frbservices.org/EPaymentsDirectory/directories/fedach?format=text"
 # }}}
 
-dl-data() { # {{{ wrap curl in function
-curl \
---header "X_FRB_EPAYMENTS_DIRECTORY_ORG_ID: 121000248" \
---header "X_FRB_EPAYMENTS_DIRECTORY_DOWNLOAD_CD: 3f2b553a-b3dc-4a2c-9213-f2b721142bbd" \
-https://frbservices.org/EPaymentsDirectory/directories/fedach?format=text > "${resultfile}"
+dl_data() { # {{{ wrap curl in function
+curl -H "${header1}" -H "${header2}" "${dl_url}" > "${resultfile}"
 } # }}}
 
 # Begin main tasks {{{
 if [[ -e "${destdir}" ]]; then
-  dl-data
+  dl_data
 else
   mkdir "${destdir}"
-  dl-data
+  dl_data
 fi
 # }}}
 
