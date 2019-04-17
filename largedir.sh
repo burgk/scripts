021d8448ca9d4bcecf314acf39c19a86737594a9
Kevin Burg <burg.kevin@gmail.com>
2019-04-17 10:48:18 -0600

    Added script largedir.sh to find largest dirs in a list

---
+++ largedir.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Purpose: Find the largest directory in a list
+# Date: 20190417
+# Kevin Burg - kevin.burg@state.co.us
+
+# Misc variable definitions {{{
+declare -a dirlist=( $(find . -maxdepth 1 -type d | cut -b 3-) )
+tmpfile="/tmp/$(date +%s)-largedir.temp"
+# }}}
+
+# Begin main tasks {{{
+for dir in "${dirlist[@]}"; do
+  cd "${dir}" || exit
+  size=$("du" -s | cut -f1) >/dev/null 2>&1
+  echo -e "${size};${dir}" >> "${tmpfile}"
+  cd .. || exit
+done
+sort -rn "${tmpfile}" | column -s";" -t
+rm "${tmpfile}"
+# }}}
+
+exit 0
