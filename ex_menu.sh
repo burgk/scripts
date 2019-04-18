ab6d105ac18ee8e49f27a4ce19af644411846d5e
Kevin Burg <burg.kevin@gmail.com>
2019-04-18 09:50:10 -0600

    Update to ex_menu.sh

--- ex_menu.sh
+++ ex_menu.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
-# Add an exec <scriptname.sh> to users .bashrc and 
+# Purpose: Example script of a menu
+# Date: 20190418
+# Kevin Burg - kevin.burg@state.co.us
+# Note: Add an exec <scriptname.sh> to users .bashrc and 
 # they will only get this menu, without the exec
 # they can exit the menu to bash. This only traps
 # Ctrl-c, other signals still work e.g. Ctrl-z
+
 trap '' 2
 while true; do
   clear
@@ -14,13 +18,19 @@ while true; do
   echo "Enter q to exit the menu q: "
   echo -e "\n"
   echo -e "Enter your selection \c"
-  read answer
-  case "$answer" in
-    1) who
-    uptime ;;
-    2) cal ;;
-    q) exit ;;
+  read -r answer
+  case "${answer}" in
+    1)
+      who
+      uptime
+    ;;
+    2)
+      cal
+    ;;
+    q)
+      exit
+    ;;
   esac
   echo -e "Enter return to continue \c"
-  read input
+  read -r input
 done
