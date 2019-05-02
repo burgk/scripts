a11a3e434195f3341b3a1bffcb93b852d5d26faa
Kevin Burg <kevin.burg@integrityforge.com>
2019-05-01 21:00:00 -0600

    Update to printf script

--- ex_printf.sh
+++ ex_printf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Purpose: printf builting practice
+# Purpose: printf building practice
 # Date: 2019-05-01
 # Kevin Burg - kevin.burg@state.co.us
 
@@ -15,25 +15,32 @@ the_mac="08:00:27:41:23:b4"
 # }}}
 printf "%12s: %4d\n" "string 1" 12 "string 2" 122
 printf "%s\n" "-----"
+
 printf "%-12s: %-4d\n" "string 1" 12 "string 2" 122
 printf "%s\n" "-----"
+
 printf "%-12s: %4d\n" "string 1" 12 "string 2" 12222
 printf "%s\n" "-----"
+
 printf "%-15s %-15s\n" "CDA" "Legislature"
 printf "%-15s %-15s\n" "CDPHE" "OIT"
 printf "%-15s %-15s\n" "CDOT" "State Capitol"
 printf "%s\n" "-----"
+
 printf -v var1 "Hello World"
 echo "$var1"
 printf "%s\n" "-----"
+
 printf "Surname: %s\nName: %s\n" "$surname" "$firstname"
 printf "%s\n" "-----"
+
 echo "$the_mac"
 the_mac="$(printf "%02x:%02x:%02x:%02x:%02x:%02x" 0x${the_mac//:/ 0x})"
 echo "$the_mac"
 the_mac="$(printf "%02X:%02X:%02X:%02X:%02X:%02X" 0x${the_mac//:/ 0x})"
 echo "$the_mac"
 printf "%s\n" "-----"
+
 for (( x=0; x <=15; x++ )); do
   printf '%3d | %04o | 0x%02x\n' "$x" "$x" "$x"
 done
