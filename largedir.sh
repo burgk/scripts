b83c1b0204312e411b699cc9b40aa72ed363646f
Kevin Burg <burg.kevin@gmail.com>
2019-05-03 10:21:23 -0600

    Re-arranged a bit

--- largedir.sh
+++ largedir.sh
@@ -1,45 +1,48 @@
 #!/usr/bin/env bash
-# Purpose: Find the largest directory in a list
-# Date: 20190417
+# Purpose: For loop example
+# Date: 2019-05-21
 # Kevin Burg - kevin.burg@state.co.us
 
 # Misc variable definitions {{{
-# mapfile -s1 -t dirlist < <(find . -maxdepth 1 -type d | cut -b 3-)
 stamp="$(date +%s)"
 tmpfile="/tmp/${stamp}-largedir.sh.tmp"
 outfile="/tmp/${stamp}-largedir.sh.out"
 pager="$(command -v less)"
 termsize="$(tput lines)"
+
 # }}}
 
 # Begin main tasks {{{
 if [[ "$#" = "0" ]]; then
-  mapfile -s1 -t dirlist < <(find . -maxdepth 1 -type d | cut -b 3-)
+  for dir in .[!.]* *; do
+    if [[ -d "${dir}" ]]; then
+      cd "${dir}" || exit
+      size=$("du" -s | cut -f1) >/dev/null 2>&1
+      echo -e "${size};${dir}" >> "${tmpfile}"
+      cd .. || exit
+    else
+      :
+    fi
+  done
 else
   echo -e "No arguments supported, this must be run from the location"
   echo -e "you want the list from.  Use of sudo or root may be"
   echo -e "required for some directories. Exiting now."
   exit 1
 fi
-  
-for dir in "${dirlist[@]}"; do
-  cd "${dir}" || exit
-  size=$("du" -s | cut -f1) >/dev/null 2>&1
-  echo -e "${size};${dir}" >> "${tmpfile}"
-  cd .. || exit
-done
 
 echo -e "Size  in  KB;Directory" > "${outfile}"
 echo -e "---m--g--t--;---------" >> "${outfile}"
 sort -rn "${tmpfile}" >> "${outfile}"
+rm "${tmpfile}"
 
-if [[ "${#dirlist[@]}" -ge  "${termsize}" ]]; then
+listsize=$(wc -l "${outfile}" | cut -d" " -f 1)
+if [[ "${listsize}" -ge  "${termsize}" ]]; then
   column -s";" -t < "${outfile}" | "${pager}"
 else
   column -s";" -t < "${outfile}"
 fi
 
-rm "${tmpfile}"
 rm "${outfile}"
 # }}}
 
