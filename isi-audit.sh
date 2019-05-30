d6efa50571392a8d28ef6882d5b9b9d3d6480d82
Kevin Burg <burg.kevin@gmail.com>
2019-05-30 15:54:23 -0600

    EOD update to isi-audit

--- isi-audit.sh
+++ isi-audit.sh
@@ -4,29 +4,37 @@
 # Kevin Burg - kevin.burg@state.co.us
 
 # Misc variable definitions {{{
-dateregex='^[0-9]{4}-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01]) ([0-2][0-9]:[0-5][0-9])$'
+dateregex='^[0-9]{4}-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01]) ([0-2][0-9]:[0-5][0-9])$' # date format regex
+zoneregex='(([[:upper:]]{1,}\.){1,}[[:upper:]]{1,})' # regex that matches the domain provider
+azregex='[[:digit:]]{1,}' # search location option regex
 efx400logdate="1450388912" # Earliest date on EFX400 Isilon - Thu Dec 17 14:48:32 MST 2015
 ts=$(date +%s) # time stamp
-local_os="$(uname -o)"
-nodecount="$(ls -l /ifs/.ifsvar/audit/logs | wc -l)"
-mkdir /ifs/iao-"${ts}"
+local_os="$(uname -o)" # only needed when testing on linux
+nodecount="$(ls -l /ifs/.ifsvar/audit/logs | wc -l)" # node count +1
 iaopath="/ifs/iao-${ts}" # isi-audit output path
-# zcat <nodelog.gz> | sed -e /s/,"/\>/g' | tr -d "\"" | tr -d "{}" | gzip > filter.gz
 # }}}
 
 # Functions {{{
 
-display_header(){ # {{{ Initial header
+show_header(){ # {{{ Header
+echo -e "\n"
 cat <<'HEADERMSG'
-
+**************************************************************
+**     ___             ___ __     __  ____  _ ___ __        **
+**    /   | __  ______/ (_) /_   / / / / /_(_) (_) /___  __ **
+**   / /| |/ / / / __  / / __/  / / / / __/ / / / __/ / / / **
+**  / ___ / /_/ / /_/ / / /_   / /_/ / /_/ / / / /_/ /_/ /  **
+** /_/  |_\__,_/\__,_/_/\__/   \____/\__/_/_/_/\__/\__, /   **
+**                                                /____/    **
+**************************************************************
 HEADERMSG
-# }}} End display_header
+} # }}} End display_header
 
 prompt_stime(){ # {{{ Prompt and validate supplied start date - vars: user_sdate, epoch_sdate
 valid_sdate="false"
 while [ "${valid_sdate}" = "false" ]; do
-  echo -n "Enter start date in the format YYYY-MM-DD HH:MM  "
-  read -e -r user_sdate
+  echo -e "\n"
+  read -rep "Enter start date in the format YYYY-MM-DD HH:MM: " user_sdate
   if ! [[ ${user_sdate} =~ $dateregex ]]; then
     echo -e "Invalid date format, need YYYY-MM-DD HH:MM"
   elif [[ "${local_os}" = *inux* ]]; then
@@ -58,8 +66,7 @@ done
 prompt_etime(){ # {{{ Prompt and validate supplied end date - vars: user_edate, epoch_edate
 valid_edate="false"
 while [ "${valid_edate}" = "false" ]; do
-  echo -n "Enter end date in the format YYYY-MM-DD HH:MM  "
-  read -e -r user_edate
+  read -rep "Enter end date in the format YYYY-MM-DD HH:MM: " user_edate
   if ! [[ ${user_edate} =~ $dateregex ]]; then
     echo -e "Invalid date format, need YYYY-MM-DD HH:MM"
   elif [[ "${local_os}" = *inux* ]]; then
@@ -77,9 +84,9 @@ while [ "${valid_edate}" = "false" ]; do
     if [[ $(date -j -f "%F %T" "${user_edate}":00 +%s) ]]; then # FreeBSD date format
       epoch_edate="$(date -j -f "%F %T" "${user_edate}":00 +%s)" # FreeBSD date format
         if [[ "${epoch_edate}" -gt "${ts}" ]]; then
-          echo -e "Error: Date is out of range"
+          echo -e "Error: End date is in the future"
         elif [[ "${epoch_edate}" -lt "${epoch_sdate}" ]]; then
-          echo -e "Error: Start date is before end date"
+          echo -e "Error: End date is before start date"
         else
           valid_edate="true"
         fi
@@ -88,22 +95,28 @@ while [ "${valid_edate}" = "false" ]; do
 done
 } # }}} End prompt_etime
 
-prompt_sloc(){ #{{{ Dynamic AD/Zone pairing for prompt - vars: user_zone, user_ad, zone_path
-echo -e "Building Access Zone list for cluster.."
+prompt_sloc(){ #{{{ Dynamic AD/Zone pairing - vars: user_zone, user_ad, zone_path
+if ! [[ -e "${iaopath}" ]]; then
+  mkdir "${iaopath}"
+fi
+echo -e "\n"
+echo -e "--> Building Access Zone list for cluster.. <--"
 declare -a az_list=()
 while read -r line; do az_list+=("$line"); done < <( isi zone zones list -a -z | cut -d" " -f1 | sort )
 for i in "${!az_list[@]}"; do
   touch "${iaopath}/${az_list[$i]}"
 done
-echo -e "Getting AD providers for Access Zones.."
+echo -e "--> Getting AD providers for Access Zones.. <--"
 cd "${iaopath}" || exit
 for file  in *; do
-  if [[ $(isi zone zones view "${file}" | grep -Eo '(([[:upper:]]{1,}\.){1,}[[:upper:]]{1,})') =~ (([[:upper:]]{1,}\.){1,}[[:upper:]]{1,}) ]]; then
-    mv "${file}" "${file} - "$(isi zone zones view "${file}" | grep -Eo '(([[:upper:]]{1,}\.){1,}[[:upper:]]{1,})');
+  if [[ $(isi zone zones view "${file}" | grep -Eo "${zoneregex}") =~ $zoneregex ]]; then
+    mv "${file}" "${file} - "$(isi zone zones view "${file}" | grep -Eo "${zoneregex}");
   fi
 done
-echo -e "Finding online AD providers, please wait.."
-mkdir "${iaopath}"/online
+if ! [[ -e "${iaopath}"/online ]]; then
+  mkdir "${iaopath}"/online
+fi
+echo -e "--> Finding online AD providers, please wait.. <--"
 cd "${iaopath}" || exit
 for file in *-*; do
   # if [[ $(isi auth ads view "${file##*,}" 2>/dev/null | grep -o online) == "online" ]]; then # more accurate, but significantly slower
@@ -122,7 +135,7 @@ while [ "${valid_sloc}" == "false" ]; do
     echo -e "[$((count + 1))] ${agency[$count]}"
   done
   read -rep "Enter number of selection: " user_tmp
-  if [[ "${user_tmp}" =~ [[:digit:]]{1,} ]] && [[ "${user_tmp}" -le "${arrsize}" ]]; then
+  if [[ "${user_tmp}" =~ $azregex ]] && [[ "${user_tmp}" -le "${arrsize}" ]]; then
     user_sloc=$((user_tmp - 1))
     user_param="${agency[$user_sloc]}"
     user_zone="${user_param% - *}"
@@ -134,12 +147,11 @@ while [ "${valid_sloc}" == "false" ]; do
     echo -e "Invalid selection"
   fi
 done
-} # }}} End prompt_sloc2
+} # }}} End prompt_sloc
 
 prompt_stype(){ # {{{ Prompt for search type: User or Path - vars: user_stype, user_sid, user_spath, search_param
 valid_stype="false"
 while [[ "${valid_stype}" = "false" ]]; do
-  echo -e "\n"
   read -rp "Will this search be for a [U]ser or [P]ath: " user_tmptype
   case "${user_tmptype}" in
     u | U)
@@ -164,20 +176,18 @@ while [[ "${valid_stype}" = "false" ]]; do
     ;;
     p | P)
       user_stype="Path"
+      echo -e "\n"
       cat <<'PATHMESSAGE'
-For a path search, there are a few options.
-We can perform a case insensitive
-search on a file, directory or path.
+For a path search there are a few options. We can perform
+a case insensitive search on a file, directory or path.
 
-If you want to search on a path
-please enter it with the format of:
+If you want to search on a path, please format it like:
 \path\to\search
 Do not include the \ifs\<agency> portion.
 
-Remember, the less specific your item is
-the more likely we are to get unexpected matches
-e.g. there may be a lot of files named 
-'New Text Document.txt' in any given agency.
+Remember, the less specific the search criteria are the
+more likely we are to get unexpected matches, e.g. there
+may be several 'New Text Document.txt' in any given agency.
 
 PATHMESSAGE
       read -rep "What is the file, directory or path to search: " user_spath
@@ -215,37 +225,71 @@ res_user="$(isi auth users view --zone="${user_zone}" --sid="$@" | grep -w "Name
 } # End resolve_sid }}}
 
 parse_log(){ # {{{ Pull out relevant parts of audit record for formatting
-:
+echo -e "Parsing of logs happens here.."
 } # }}} End parse_log
-
-create_share(){ # {{{ WARNING: EXPERIMENTAL - generic share creation
-echo -e "Share will be created with Everyone - Full Control SMB permissions"
-read -rep "Share Access Zone: " user_shareaz
-read -rep "Share name: " user_sharename
-read -rep "Share description: " user_sharedescription
-read -rep "Share path: " user_sharepath
-isi smb shares create "${user_sharename}" "${user_sharepath}" --create-path --description="${user_sharedescription}" --zone="${user_shareaz}" 
-isi smb shares permission "${user_sharename}" --wellknown Everyone --permission full --zone="${user_shareaz}"
-} # }}} End create share
 
 # }}} End functions section
 
 # Begin main tasks  {{{
-prompt_stime
-prompt_etime
-prompt_sloc
-prompt_stype
-generate_logs
-echo -e "\nStart is: ${user_sdate}"
-echo -e "End is: ${user_edate}"
-echo -e "Search type is: ${user_stype}"
-echo -e "Search location is: ${user_zone}"
-if [[ "${user_stype}" = "User" ]]; then
-  echo -e "Search criteria: ${user_suser}"
-  echo -e "User resolves to: ${search_param}"
-else
-  echo "Search criteria: ${search_param}"
-fi
+show_header
+user_confirm="false"
+while [ "${user_confirm}" == false ]; do
+  prompt_stime
+  prompt_etime
+  prompt_sloc
+  prompt_stype
+  echo -e "\nStart date/time is: ${user_sdate}"
+  echo -e "End date/time is: ${user_edate}"
+  echo -e "Search type is: ${user_stype}"
+  echo -e "Search location is: ${user_zone}"
+  if [[ "${user_stype}" = "User" ]]; then
+    echo -e "Search criteria: ${user_suser}"
+  else
+    echo "Search criteria: ${search_param}"
+  fi
+  read -rep "Do these search criteria look correct [y|n]? " user_confirm_response
+  case "${user_confirm_response}" in
+    y | Y)
+    echo -e "Ok, continuing.."
+    generate_logs
+    parse_log
+    user_confirm="true"
+    ;;
+    n | N)
+    echo -e "\nOk, which item needs changed?"
+    echo -e "[1] Start date/time"
+    echo -e "[2] End date/time"
+    echo -e "[3] Search location"
+    echo -e "[4] Search type"
+    read -rep "Enter selection: " redo_function
+    case "${redo_function}" in
+      1)
+      prompt_stime
+      continue
+      ;;
+      2)
+      prompt_etime
+      continue
+      ;;
+      3)
+      prompt_sloc
+      continue
+      ;;
+      4)
+      prompt_stype
+      continue
+      ;;
+      *)
+      echo -e "Error: Invalid response selected"
+      ;;
+    esac
+    ;;
+    *)
+    echo -e " Error: Invalid response selected"
+    ;;
+  esac
+done
+echo -e "Complete"
 # }}}
 
 exit 0
