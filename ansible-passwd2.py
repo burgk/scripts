61fd62d3d9ba70f7e592e5d2a11f5c735170fabd
Kevin Burg <kevin.burg@integrityforge.com>
2020-12-17 09:35:45 -0700

    Correct ansible-passwd2 to reflect python2

--- ansible-passwd2.py
+++ ansible-passwd2.py
@@ -1,2 +1,2 @@
-python3 -c "import crypt, getpass, pwd; print(crypt.crypt(input(), crypt.mksalt(crypt.METHOD_SHA512)))"
+python2 -c "import crypt, getpass, pwd; print crypt.crypt(raw_input(), crypt.mksalt(crypt.METHOD_SHA512))"
 
