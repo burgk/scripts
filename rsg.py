5cbbe4f36213fbab4063aaa5ceb546677b773644
Kevin Burg <kevin.burg@integrityforge.com>
2020-12-31 16:16:48 -0700

    Corrected to run under python 3

--- rsg.py
+++ rsg.py
@@ -14,7 +14,7 @@ while second_prize == first_prize:
 third_prize = random.randrange(0,len(entrants))
 while third_prize == first_prize or third_prize == second_prize:
     third_prize = random.randrange(0,len(entrants))
-print "Selections are done and:"
-print "1st Prize, a Raspberry Pi 3, goes to", entrants[first_prize]
-print "2nd Prize, a Raspberry Pi Zero, goes to", entrants[second_prize]
-print "3rd Prize, a gift card to Micro Center, goes to", entrants[third_prize]
+print("Selections are done and:")
+print("1st Prize, a Raspberry Pi 3, goes to"), entrants[first_prize]
+print("2nd Prize, a Raspberry Pi Zero, goes to"), entrants[second_prize]
+print("3rd Prize, a gift card to Micro Center, goes to"), entrants[third_prize]
