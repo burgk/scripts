d7d5bcde6f2a13a8f908f5e1d6d5b4ca77f315ac
Kevin Burg <burg.kevin@gmail.com>
2021-02-23 12:10:09 -0700

    Adjusted to use vars for agency and admgroup

--- isi-perms.sh
+++ isi-perms.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+admgrp="cdhs_admins"
+zone="CDHSHIPAA"
 declare -a sharelist=()
 
-while read -r line; do sharelist+=("$line"); done < <( isi smb shares list --zone=CDOT -a -z | awk -F'/' '{ print $1 }' | sed 's/ *$//' )
+while read -r line; do sharelist+=("$line"); done < <( isi smb shares list --zone="${zone}" -a -z | awk -F'/' '{ print $1 }' | sed 's/ *$//' )
 
 for share in "${!sharelist[@]}"; do
   echo -e "\nChecking: ${sharelist[$share]}"
-  isi smb shares view "${sharelist[$share]}" --zone=CDOT | grep CDOT_Admins 2>&1 > /dev/null
+  isi smb shares view "${sharelist[$share]}" --zone="${zone}" | grep "${admgrp}" 2>&1 > /dev/null
   if [[ $? == 0 ]]; then
-    echo -e "${sharelist[$share]} has CDOT_Admins checking for run-as-root.."
-    isi smb shares view "${sharelist[$share]}" --zone=CDOT | grep CDOT_Admins | grep True 2>&1 > /dev/null
+    echo -e "${sharelist[$share]} has "${admgrp}" checking for run-as-root.."
+    isi smb shares view "${sharelist[$share]}" --zone="${zone}" | grep "${admgrp}" | grep True 2>&1 > /dev/null
     if [[ $? == 0 ]]; then
-      echo -e "${sharelist[$share]} has CDOT_Admins with run-as-root, skipping.."
+      echo -e "${sharelist[$share]} has "${admgrp}" with run-as-root, skipping.."
     else
-      echo -e "${sharelist[$share]} has CDOT_Admins without run-as-root, fixing.."
+      echo -e "${sharelist[$share]} has "${admgrp}" without run-as-root, fixing.."
       echo -e "--running perms modify cmd here--"
-      isi smb shares permission modify "${sharelist[$share]}" --group CDOT_Admins --run-as-root --zone=CDOT
+      isi smb shares permission modify "${sharelist[$share]}" --group "${admgrp}" --run-as-root --zone="${zone}"
     fi
   else
-    echo -e "${sharelist[$share]} does NOT have CDOT_Admins, adding.."
+    echo -e "${sharelist[$share]} does NOT have "${admgrp}", adding.."
     echo -e "--running perms add cmd here--"
-     isi smb shares permission create "${sharelist[$share]}" --group CDOT_Admins --run-as-root --zone=CDOT
+     isi smb shares permission create "${sharelist[$share]}" --group "${admgrp}" --run-as-root --zone="${zone}"
   fi
 done
